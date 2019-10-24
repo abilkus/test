d6e1efa28483fa49c3219a056745c3570bb02811
abilkus <48215890+abilkus@users.noreply.github.com>
2019-10-24 10:48:04 +0100

    Add files via upload

--- learning.py
+++ learning.py
@@ -12,3 +12,14 @@ movie_data.groupby('title')['rating'].count().sort_values(ascending=False).head(
 ratings_mean_count = pd.DataFrame(movie_data.groupby('title')['rating'].mean())
 ratings_mean_count['rating_counts'] = pd.DataFrame(movie_data.groupby('title')['rating'].count())
 ratings_mean_count.head()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('dark')
+%matplotlib inline
+
+plt.figure(figsize=(8,6))
+plt.rcParams['patch.force_edgecolor'] = True
+sns.jointplot[x='rating', y='rating_counts', data=ratings_mean_count, alpha=0.4]
+user_movie_rating = movie_data.pivot_table(index='userId', columns='title', values='rating')
+user_movie_rating.head()
